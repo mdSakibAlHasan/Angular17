3d3439922e8b48f9b58c8d7d249a2159ae44aac7
Sakib Al Hasan <bsse1209@iit.du.ac.bd>
2024-01-19 15:14:50 +0600

    signal object array update complete

--- src/app/signal/signal.component.ts
+++ src/app/signal/signal.component.ts
@@ -1,20 +1,65 @@
-import { Component, computed, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, computed, signal } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signal',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './signal.component.html',
   styleUrl: './signal.component.css'
 })
-export class SignalComponent {
+export class SignalComponent implements OnInit {
   firstName = signal('sakib');
   lastName = signal('al hasan');
   finame:string=""
   laname:string=""
+  userForm:any;
+  employee = signal<object[]>([
+    {name:'sakib', email:'bsse1209'},
+    {name:'hasan',email:'bsse1212'}
+  ]);
 
   fullName = computed(() => this.firstName()+ ' ' + this.lastName())
-  // fuName = computed(()=> this.finame + " "+ this.laname);
+
+  constructor(private formBuilder:FormBuilder){}
+  employeeForm!: FormGroup;
+  
+  employees = signal([
+    { name: 'John Smith', position: 'Manager', department: 'Sales' },
+    { name: 'Jane Doe', position: 'Engineer', department: 'Engineering' },
+    { name: 'Bob Johnson', position: 'Marketing Specialist', department: 'Marketing' },
+    { name: 'Sarah Lee', position: 'HR Manager', department: 'Human Resources' },
+    { name: 'Michael Brown', position: 'IT Support', department: 'IT' }
+  ]);
+
+  empAdding= signal<boolean>(false);
+
+  toggleAddBlock(){
+    this.empAdding.update(status => !status);
+  }
+  
+  
+  ngOnInit() {
+    this.employeeForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      position: ['', Validators.required],
+      department: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    this.employees.update(empList => [...empList, this.employeeForm.value]);
+    this.employeeForm.reset()
+   // Update the position of the employee at index 1 to 'Software Engineer'
+this.employees.update(value => [
+  ...value.slice(0,1),
+  { ...value[1], position: 'Software Engineer' },
+  ...value.slice(2)
+]);
+
+  }
+  
 
   changeFirstName(name:string){
     console.log(name);
